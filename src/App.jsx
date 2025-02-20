615e56e674a50346fd5b9ad8ce9c3e7711943f97
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-02-20 12:54:52 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react';
+import './App.css';
+import Profile from './components/Profile/Profile';
+import userData from './userData.json';
+import friends from './friends.json';
+import FriendList from './components/FriendList/FriendList';
+import transactions from './transactions.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
+function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
+
+export default App;
